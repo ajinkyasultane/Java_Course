049996719f08dd4bd4a3cf62931e85f6fd967f10
ajinkyasultane <ajinkyasultane@gmail.com>
2025-08-25 18:48:54 +0530

    Day 4th ternary_operator.

---
+++ src/Ternary_Operator.java
@@ -0,0 +1,12 @@
+public class Ternary_Operator {
+    public static void main(String args[])
+    {
+        //Ternary operator
+
+        int i = 190;
+
+        String j  = (i == 10) ? " Hello " : "Good Bye";
+        System.out.println(j);
+    }
+
+}
