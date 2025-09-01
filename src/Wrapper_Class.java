9a35064c6fef573df632c758fed5aeb3a711c105
ajinkyasultane <ajinkyasultane@gmail.com>
2025-09-01 10:07:57 +0530

    Day 11th Wrapper class

---
+++ src/Wrapper_Class.java
@@ -0,0 +1,13 @@
+public class Wrapper_Class {
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt("123");
+
+        char letter = 'a';
+
+        System.out.println(Character.isLetter(letter));
+        System.out.println(Character.isUpperCase(letter));
+      //  System.out.println(Integer.SIZE);
+    }
+
+}
