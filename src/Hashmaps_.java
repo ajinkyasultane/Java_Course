400e0030cb358be7065df6a2ec49f589f47f6144
ajinkyasultane <ajinkyasultane@gmail.com>
2025-09-02 18:31:26 +0530

    Day 12th Hashmap

---
+++ src/Hashmaps_.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Hashmaps_ {
+    public static void main(String[] args)
+    {
+        HashMap<String,Double> map = new HashMap<>();
+
+        map.put("Pineapple", 49.99);
+        map.put("Apple",24.99);
+        map.put("Banana" , 10.21);
+        map.put("Mango", 15.50);
+
+        System.out.println(map );
+
+    }
+}
